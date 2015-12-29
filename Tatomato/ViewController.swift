ec0afd80d3e30372981e1f3920b766cf3050a827
TAmbition <tambition@mail.com>
2015-12-29 22:42:46 +0800

    Finish simple UI

---
+++ Tatomato/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tatomato
+//
+//  Created by 胡雨阳 on 15/12/28.
+//  Copyright © 2015年 胡雨阳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
